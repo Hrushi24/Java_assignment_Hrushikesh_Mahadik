190030db9662e17bbe9123230931074e874a75ae
Hrushi24 <hrushikeshjmahadik247@gmail.com>
2024-03-12 07:09:29 +0530

    Q18. Program to print all odds from 1 to 100.
    Q18. Program to print all odds from 1 to 100.

---
+++ Java_Assignment_Hrushikesh_Mahadik/src/java_assignment_till_array/Q18_all_odd_1_to_100.java
@@ -0,0 +1,24 @@
+package java_assignment_till_array;
+
+public class Q18_all_odd_1_to_100 {
+
+	public static void main(String[] args) {
+
+		System.out.println("All the even numbers between 1 to 100 .");
+		
+		for(int i = 1; i <=100; i++)
+		{
+			if(i%2==1) 
+			{
+				System.out.print(i + " ");
+				
+			}
+			if(i%20==0)
+			{
+				System.out.println();
+			}
+		}
+
+	}
+
+}
